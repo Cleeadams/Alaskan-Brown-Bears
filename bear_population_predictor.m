c69e5f8815e327edc6fe3570a77a90ce8987d9dd
Cleeadams <90994614+Cleeadams@users.noreply.github.com>
2022-02-01 19:46:01 -0800

    Update bear_population_predictor.m

--- bear_population_predictor.m
+++ bear_population_predictor.m
@@ -10,7 +10,7 @@ clc
   % bears is 30,000.
 N = 30000;
   
-  % Gender proportions
+  % Gender proportions - (Wieglus and Bunnell, 1994)
 m = .3;
 f = .22;
 sm = .22;
@@ -18,33 +18,33 @@ sf = .11;
 c = .15;
 
   % Harvest rates
+    % The harvest rate seems to be included with the mortality rates already.
 h = [0, .05, .067, .1];
 
   % Mortality rates
     % Cubs (0.5 - 1.5)
-Cub_M = [.3, .4];
+Cub_M = [.3, .4, .22];
     % Subadults
-Sub_M = .44;
-Sub_F = .11;
+Sub_M = [.44, .21];
+Sub_F = [.11, .7];
     % Adult mortality rate (> 5)
-Male_M = .19;
-Female_M = .13;
+Male_M = [.19, .28, .3];
+Female_M = [.13,.7];
 
 
   % Reproductive rate for young adult females.
-r = [.7, .76];
+r = [.7, .735, .76, .46];
 
   % Prediction over 10 years
 time = 10;
 
-
+    % Check to see if the article lines up with sf + f.
 for i = 2:time
-  N(i) = N(i-1) * ( 1 + r(2) * (sf + f) ) - N(i-1) * ( h(1) + c * Cub_M(1)...
-     + sm * Sub_M + sf * Sub_F + m * Male_M + f * Female_M )
+  N(i) = N(i-1) * ( 1 + r(2) * (sf+f) ) - N(i-1) * ( c * Cub_M(1)...
+     + sm * Sub_M(1) + sf * Sub_F(1) + m * Male_M(1) + f * Female_M(1) )
 end
 
 
-    
 plot(1:time, N)
 grid on
 
