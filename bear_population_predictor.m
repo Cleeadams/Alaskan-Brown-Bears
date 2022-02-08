3f69cb428470ea575e78ca6e9cf4e8c905fbf7fd
Cleeadams <90994614+Cleeadams@users.noreply.github.com>
2022-02-07 16:20:51 -0800

    Update bear_population_predictor.m

--- bear_population_predictor.m
+++ bear_population_predictor.m
@@ -1,4 +1,4 @@
-% Population model
+ % Population model
 
 close all
 clear 
@@ -13,8 +13,8 @@ N = 30000;
   % Gender proportions - (Wieglus and Bunnell, 1994)
 m = .3;
 f = .22;
-sm = .22;
-sf = .11;
+sm = .295;
+sf = .185;
 c = .15;
 
   % Harvest rates
@@ -23,29 +23,39 @@ h = [0, .05, .067, .1];
 
   % Mortality rates
     % Cubs (0.5 - 1.5)
-Cub_M = [.3, .4, .22];
+Cub_M = .22;
     % Subadults
-Sub_M = [.44, .21];
-Sub_F = [.11, .7];
+Sub_M = .44;
+Sub_F = .11;
     % Adult mortality rate (> 5)
-Male_M = [.19, .28, .3];
-Female_M = [.13,.7];
+Male_M = .15;
+Female_M = .07;
 
 
   % Reproductive rate for young adult females.
-r = [.7, .735, .76, .46];
+r = .42;
 
   % Prediction over 10 years
-time = 10;
+time = 11;
 
     % Check to see if the article lines up with sf + f.
+##for i = 2:time
+##  N(i) = N(i-1) * ( 1 + r(2) * (sf+f) ) - N(i-1) * ( c * Cub_M(1)...
+##     + sm * Sub_M(1) + sf * Sub_F(1) + m * Male_M(1) + f * Female_M(1) )
+##end
+
 for i = 2:time
-  N(i) = N(i-1) * ( 1 + r(2) * (sf+f) ) - N(i-1) * ( c * Cub_M(1)...
-     + sm * Sub_M(1) + sf * Sub_F(1) + m * Male_M(1) + f * Female_M(1) )
+  N(i) = N(i-1) * ( 1 + r * (sf+f) ) - N(i-1) * ( sm * Sub_M...
+     + sf * Sub_F + m * Male_M + f * Female_M )
 end
 
-
-plot(1:time, N)
+figure(1)
+plot(0:time-1, N, "linewidth", 2)
+xlabel("Time (yrs)", 'FontSize', 25)
+ylabel("Population of Brown Bears", 'FontSize', 25)
+title("The Population of Alaskan Brown Bears", 'FontSize', 25)
 grid on
 
-N(10)/N(1)
+gr = N(time)/N(1)
+1-gr
+
